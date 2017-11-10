78664736801911841efecfad90240729a954a1b2
Evan Arch <evantarch@gmail.com>
2017-11-10 12:56:05 -0700

    Screen Size

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package com.mygdx.game.desktop;
 
+import java.awt.Toolkit;
+import java.awt.Dimension;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.DustGame;
@@ -7,6 +9,11 @@ import com.mygdx.game.DustGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "DUST";
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		config.width = (int) screenSize.getWidth();
+		config.height = (int) screenSize.getHeight();
+
 		new LwjglApplication(new DustGame(), config);
 	}
 }
