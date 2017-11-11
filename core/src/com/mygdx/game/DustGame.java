534725a213f404160720fcd9a7156a202c297bd6
Evan Arch <evantarch@gmail.com>
2017-11-11 01:52:14 -0700

    Screen Size

--- core/src/com/mygdx/game/DustGame.java
+++ core/src/com/mygdx/game/DustGame.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import java.util.*;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
@@ -20,11 +21,14 @@ public class DustGame extends ApplicationAdapter {
 	SpriteBatch player;
 	Texture playerI;
 	TextureRegion playerSheetPos;
-	Texture monsterTexture;
 	SpriteBatch dagger;
 	Texture daggerI;
-	static final int WORLD_WIDTH = 1000;
-	static final int WORLD_HEIGHT = 1000;
+	Texture voidConnector;
+	Texture verticalVoidBridge;
+	Texture horizontalVoidBridge;
+	ArrayList bridges = new ArrayList();
+	static final int WORLD_WIDTH = 500;
+	static final int WORLD_HEIGHT = 500;
 	OrthographicCamera cam;
 	SpriteBatch world;
 	Sprite mapSprite;
@@ -36,29 +40,29 @@ public class DustGame extends ApplicationAdapter {
 	public float playerYpos;
 	int playerXTextOffset;
 	int playerYTextOffset;
-	Array<Rectangle> monsters;
-	long lastSpawnTime;
+
 
 
 
 	@Override
 	public void create () {
-		mapSprite = new Sprite(new Texture(Gdx.files.internal("REALGROUND.png")));
-		mapSprite.setPosition(-500, -500);
+		mapSprite = new Sprite(new Texture(Gdx.files.internal("AAAAAAA.png")));
+		mapSprite.setPosition(WORLD_WIDTH/-2, WORLD_HEIGHT/-2);
 		mapSprite.setSize(WORLD_WIDTH, WORLD_HEIGHT);
 		w = Gdx.graphics.getWidth();
 		h = Gdx.graphics.getHeight();
 		cam = new OrthographicCamera(30, 30 * (h / w));
 		cam.update();
+		verticalVoidBridge = new Texture("VerticalVoidBridge.png");
+		horizontalVoidBridge = new Texture("HorizontalVoidBridge.png");
+		voidConnector = new Texture("VoidBridgeConnector.png");
 		world = new SpriteBatch();
-		monsterTexture = new Texture("monster.png");
 		player = new SpriteBatch();
 		playerI = new Texture("CCsprite.png");
 		playerSheetPos = new TextureRegion(playerI, 66, 0, 22, 38);
 		dagger = new SpriteBatch();
 		daggerI = new Texture("THETRUEKNIFE.png");
-		monsters = new Array<Rectangle>();
-		spawnAttempt();
+
 	}
 
 	@Override
@@ -76,22 +80,11 @@ public class DustGame extends ApplicationAdapter {
 		playerXpos = (cameraXPos + cameraYPos + screen.getScreenWidth())/2 -11;
 		playerYpos = (cameraYPos + cameraYPos + screen.getScreenHeight())/2 - 19;
 		world.setProjectionMatrix(cam.combined);
-		spawnAttempt();
-		Iterator<Rectangle> iter = monsters.iterator();
-		while(iter.hasNext()){
-			Rectangle monster = iter.next();
-			monster.x += playerXpos/6;
-			monster.y += playerYpos/6;
-		}
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		world.begin();
-		for(Rectangle monster: monsters){
-			world.draw(monsterTexture, monster.x, monster.y);
-		}
 		mapSprite.draw(world);
 		world.end();
-
 		player.begin();
 		player.draw(playerSheetPos, playerXpos, playerYpos);
 		player.end();
@@ -118,40 +111,6 @@ public class DustGame extends ApplicationAdapter {
 			cam.translate((float) -0.3, 0, 0);
 			playerXTextOffset = 0;
 		}
-	}
-
-	void spawnAttempt(){
-		if(TimeUtils.millis() - lastSpawnTime > 1000000000){
-			boolean isSpawnXValid = false;
-			boolean isSpawnYValid = false;
-			float spawnX = MathUtils.random(playerXpos - 50, playerXpos + 50);
-			float spawnY = MathUtils.random(playerYpos - 50, playerYpos + 50);
-			while(isSpawnXValid == false && isSpawnYValid == false){
-				if(spawnX >= (playerXpos - 30) && (spawnX <= (playerXpos + 30))){
-					spawnX = MathUtils.random(playerXpos - 50, playerXpos + 50);
-				}
-				else{
-					isSpawnXValid = true;
-				}
-				if(spawnY >= (playerYpos - 30) && (spawnY <= (playerYpos + 30))){
-					spawnY = MathUtils.random(playerYpos - 50, playerYpos + 50);
-				}
-				else{
-					isSpawnYValid = true;
-				}
-			}
-			spawnMonster(spawnX, spawnY);
-		}
-	}
-
-	void spawnMonster(float monsterSpawnX, float monsterSpawnY){
-		Rectangle monster = new Rectangle();
-		monster.x = monsterSpawnX;
-		monster.y = monsterSpawnY;
-		monster.height = 16;
-		monster.width = 9;
-		monsters.add(monster);
-		lastSpawnTime = TimeUtils.millis();
 	}
 
 	@Override
